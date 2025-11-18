a0d9245ba85f449ccad80839feaec7048fa9e3c2
ASSAL Hatim <hatim931@icloud.com>
2025-11-18 08:59:43 +0100

    Spring Security simple configuration

---
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/config/SpringSecurityConfig.java
@@ -0,0 +1,38 @@
+package be.iccbxl.pid.reservationsspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCryptVersion.$2Y, 12);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/").permitAll();
+                auth.requestMatchers("/login", "/login**", "/css/**", "/js/**").permitAll();
+                auth.requestMatchers("/admin").hasRole("ADMIN");
+                auth.requestMatchers("/user").hasRole("MEMBER");
+                auth.anyRequest().authenticated();
+            })
+            .formLogin(form -> form
+                .loginPage("/login")
+                .usernameParameter("login")
+                .failureUrl("/login?loginError=true")
+                .permitAll())
+            .logout(logout -> logout.permitAll())
+            .build();
+    }
+}
